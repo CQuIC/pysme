0486cf2f9b44f13bf0d930083f42a074f535d197
Jonathan A. Gross <azaghalofbelegost@gmail.com>
2014-07-09 18:04:21 -0600

    Added file for building SDE systems.

---
+++ system_builder.py
@@ -0,0 +1,7 @@
+"""
+.. module:: system_builder.py
+   :synopsis: Build the matrix representation of the system of coupled
+   real-valued stochastic differential equations.
+.. moduleauthor:: Jonathan Gross <jarthurgross@gmail.com>
+
+"""
