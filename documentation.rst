afe604d6f6ceee5fa13a3e03872fa99f8ac1c605
Jonathan A. Gross <azaghalofbelegost@gmail.com>
2014-08-07 14:06:42 -0600

    Fixed errors with length of basis lists and corrected some documentation

--- documentation.rst
+++ documentation.rst
@@ -1,14 +1,30 @@
 Code documentation
 ==================
 
+system_builder
+--------------
+
 .. automodule:: system_builder
+   :synopsis:
    :members:
+
+gellmann
+--------
 
 .. automodule:: gellmann
+   :synopsis:
    :members:
+
+gramschmidt
+-----------
 
 .. automodule:: gramschmidt
+   :synopsis:
    :members:
 
+integrate
+---------
+
 .. automodule:: integrate
+   :synopsis:
    :members:
--- integrate.py
+++ integrate.py
@@ -1,5 +1,5 @@
 """
-.. module:: integrate.py
+.. py:module:: integrate.py
    :synopsis: Integrate stochastic master equations in vectorized form.
 .. moduleauthor:: Jonathan Gross <jarthurgross@gmail.com>
 
@@ -10,13 +10,15 @@ from scipy.integrate import odeint
 from pysme.system_builder import *
 
 def uncond_vac_integrate(rho_0, c_op, basis, times):
-    """Integrate an unconditional vacuum master equation.
+    r"""Integrate an unconditional vacuum master equation.
 
     :param rho_0:   The initial state of the system
     :type rho_0:    numpy.array
     :param c_op:    The coupling operator
     :type c_op:     numpy.array
     :param basis:   The Hermitian basis to vectorize the operators in terms of
+                    (with the component proportional to the identity in last
+                    place)
     :type basis:    list(numpy.array)
     :param times:   A sequence of time points for which to solve for rho
     :type times:    list(real)
@@ -25,14 +27,15 @@ def uncond_vac_integrate(rho_0, c_op, basis, times):
     :rtype:         list(numpy.array)
 
     """
+
     rho_0_vec = [comp.real for comp in vectorize(rho_0, basis)]
-    diff_mat = diffusion_op(c_op, basis)
+    diff_mat = diffusion_op(c_op, basis[:-1])
     
     return odeint(lambda rho_vec, t: np.dot(diff_mat, rho_vec), rho_0_vec,
             times, Dfun=(lambda rho_vec, t: diff_mat))
 
 def uncond_gauss_integrate(rho_0, c_op, M_sq, N, H, basis, times):
-    """Integrate an unconditional Gaussian master equation.
+    r"""Integrate an unconditional Gaussian master equation.
 
     :param rho_0:   The initial state of the system
     :type rho_0:    numpy.array
@@ -45,6 +48,8 @@ def uncond_gauss_integrate(rho_0, c_op, M_sq, N, H, basis, times):
     :param H:       The plant Hamiltonian
     :type H:        numpy.array
     :param basis:   The Hermitian basis to vectorize the operators in terms of
+                    (with the component proportional to the identity in last
+                    place)
     :type basis:    list(numpy.array)
     :param times:   A sequence of time points for which to solve for rho
     :type times:    list(real)
@@ -53,10 +58,12 @@ def uncond_gauss_integrate(rho_0, c_op, M_sq, N, H, basis, times):
     :rtype:         list(numpy.array)
 
     """
+
     rho_0_vec = [comp.real for comp in vectorize(rho_0, basis)]
-    diff_mat = (N + 1)*diffusion_op(c_op, basis) + \
-                N*diffusion_op(c_op.conj().T, basis) +
-                double_comm_op(c_op, M_sq, basis) + hamiltonian_op(H, basis)
+    diff_mat = (N + 1)*diffusion_op(c_op, basis[:-1]) + \
+            N*diffusion_op(c_op.conj().T, basis[:-1]) + \
+            double_comm_op(c_op, M_sq, basis[:-1]) + hamiltonian_op(H,
+                    basis[:-1])
     
     return odeint(lambda rho_vec, t: np.dot(diff_mat, rho_vec), rho_0_vec,
             times, Dfun=(lambda rho_vec, t: diff_mat))
