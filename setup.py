c3578994f4c006f5435492e9f54693e4143790de
Jonathan A. Gross <jarthurgross@gmail.com>
2015-11-28 13:07:00 -0700

    Restructured module to have organized directories

--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
 from setuptools import setup
 
 requires = [
-    'numpy',
-    ]
+        'Cython',
+        'numpy',
+        'scipy',
+        'sympy',
+        'joblib',
+         ]
 
 setup(name='pysme',
-    version='0.0',
-    py_modules=['gellmann'],
-    install_requires=requires,
-    )
+      version='0.1',
+      py_modules=['gellmann', 'gramschmidt', 'grid_conv', 'integrate', 'sde',
+                  'system_builder'],
+      install_requires=requires,
+      # Workaround from
+      # https://github.com/numpy/numpy/issues/2434#issuecomment-65252402
+      # and
+      # https://github.com/h5py/h5py/issues/535#issuecomment-79158166
+      setup_requires=['numpy', 'Cython'],
+      packages=['pysme'],
+      package_dir={'pysme': 'src/pysme'},
+     )
