df7b1245519d08e730a0fb48ca9e999ad4b195d4
Jonathan A. Gross <jarthurgross@gmail.com>
2018-01-01 17:36:27 +1000

    Add new modules to __init__.py

--- src/pysme/__init__.py
+++ src/pysme/__init__.py
@@ -2,5 +2,7 @@ from . import gellmann
 from . import gramschmidt
 from . import grid_conv
 from . import integrate
+from . import matrix_form
+from . import projector_method
 from . import sde
 from . import system_builder
