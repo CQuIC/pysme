acc09044567e6c7f195a9111d8e7bc75d1f1cbca
Jonathan A. Gross <jarthurgross@gmail.com>
2018-04-17 20:10:41 -0600

    Add matrize method and new superop matrix

--- src/pysme/sparse_system_builder.py
+++ src/pysme/sparse_system_builder.py
@@ -47,6 +47,37 @@ class SparseBasis:
     def dualize(self, op):
         return np.conj(self.vectorize(op)) * self.sq_norms
 
+    def matrize(self, vec):
+        """Take a (sparse) vectorized operator and return it in matrix form.
+
+        """
+        return sparse.tensordot(self.basis, vec, ([0], [0]))
+
+    def make_real_sand_matrix(self, x, y):
+        r"""Make the superoperator matrix representation of
+
+        N[X,Y](rho) = (1/2) ( X rho Y† + Y rho X† )
+
+        In the basis {Λ_j}, N[X,Y](rho) = N(x,y)_jk rho_k Λ_j where
+
+        N(x,y)_jk = Re[x_m (D_mlj + iF_mlj) (y*)_n (D_knl + iF_knl) ]
+
+        Λ_j Λ_k = (D_jkl + iF_jkl) Λ_l
+
+        x and y are vectorized representations of the operators X and Y stored
+        in sparse format.
+
+        `sparse.tensordot` might decide to return something dense, so the user
+        should be aware of that.
+
+        """
+        return sparse_real(sparse.tensordot(
+                                sparse.tensordot(x, self.struct,
+                                                 ([0], [0])),
+                                sparse.tensordot(np.conj(y), self.struct,
+                                                 ([0], [1])),
+                                ([0], [1])))
+
     def make_real_comm_matrix(self, x, y):
         r"""Make the superoperator matrix representation of
 
@@ -54,7 +85,7 @@ class SparseBasis:
 
         In the basis {Λ_j}, M[X,Y](rho) = M(x,y)_jk rho_k Λ_j where
 
-        M(x,y)_jk = -2 Im[x_m (y*)_n F_lnj (D_mkl + iF_mkl)]
+        M(x,y)_jk = -2 Im[ (y*)_n F_lnj x_m (D_mkl + iF_mkl) ]
 
         Λ_j Λ_k = (D_jkl + iF_jkl) Λ_l
 
